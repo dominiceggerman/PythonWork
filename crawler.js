1b2ec3c43a8a52e24bc0f277187bb8d6ba674041
dominiceggerman <dominiceggerman@gmail.com>
2018-08-13 15:47:09 -0500

    started crawler script

---
+++ crawler.js
@@ -0,0 +1,9 @@
+// By Dominic Eggerman
+// Crawler to fetch data for locroles
+
+// Nav to page
+window.location = "http://gcc.genscape.com/GCCcontent/intranet/manual_normalization.php";
+
+// Select source
+sources = document.getElementById("selFFPipeline");
+table = document.getElementById("reportResultTable");
