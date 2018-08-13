b5fe530b93802f50475fd35cf4562773dfd0884d
dominiceggerman <dominiceggerman@gmail.com>
2018-08-13 09:49:32 -0500

    checking commits.

--- gui-demo.py
+++ gui-demo.py
@@ -7,20 +7,20 @@ def btnClicked():
 if __name__ == "__main__":
 
     # Imports
-    from tkinter import *
+    import tkinter as tk
 
-
-    window = Tk()
+    # Build window
+    window = tk.Tk()
     window.title("GUI interface")
     window.geometry("600x400")
 
-    lbl = Label(window, text="Hello", font=("Arial Bold", 40))
+    lbl = tk.Label(window, text="Hello", font=("Arial Bold", 40))
     lbl.grid(column=0, row=0)
 
-    txt = Entry(window, width=10)
+    txt = tk.Entry(window, width=10)
     txt.grid(column=1, row=0)
 
-    btn = Button(window, text="button", bg="green", fg="blue", command=btnClicked)
+    btn = tk.Button(window, text="button", bg="green", fg="blue", command=btnClicked)
     btn.grid(column=2, row=0)
 
     # Run window
