2687fa5811ac66620c36ad2767ab9bf11782ce1c
dominiceggerman <dominiceggerman@gmail.com>
2018-08-14 13:17:22 -0500

    Implemented dataset selection.

--- crawler.js
+++ crawler.js
@@ -5,21 +5,34 @@
 window.location = "http://gcc.genscape.com/GCCcontent/intranet/manual_normalization.php";
 
 // Select form
-mainForm = document.getElementsByName("frmMain")[0]
+mainForm = document.getElementsByName("frmMain")[0];
 
 // Select pipeline
 pipeMenu = document.getElementById("selFFPipeline");
-pipeMenu.value = 248
+pipeMenu.value = 248;
 
-// Grab all the pipeline ids and associated indexes
-for (var i = 0; i <= pipeMenu.length; i++) {
-    pipeMenu.selectedIndex = i;
-    var pipeID = pipeMenu.value;
-    console.log(i + ":" + pipeID)
+////////////////////////
+// Select opavail run //
+////////////////////////
+// Get the data table
+var table = document.getElementById("reportResultTable")
+// Check which input has manual run = True
+var numOps = table.children[1].children.length;
+// Instantiate input
+var input
+// Loop to get the correct input
+for (var i = 0; i <= numOps; i++) {
+    var args = table.children[1].children[i].children[2].textContent;
+    if (args.includes("manualRun=true")) {
+        // Get input button and break
+         input = table.children[1].children[i].children[0].children[0];
+         break;
+    }
 }
 
-// Select opavail run
-table = document.getElementById("reportResultTable");
+// Mimic click on input
+input.click()
 
 // Fetch
-fetcher = document.getElementsByName("cmdFF")[0]
+var fetcher = document.getElementById("cmdFF");
+fetcher.click();
