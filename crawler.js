3696ea4fd39e9a6b39f2926da4466621cceba2fb
dominiceggerman <dominiceggerman@gmail.com>
2018-08-14 12:28:28 -0500

    more work on crawler

--- crawler.js
+++ crawler.js
@@ -1,9 +1,25 @@
 // By Dominic Eggerman
 // Crawler to fetch data for locroles
 
-// Nav to page
+// Navigate to page
 window.location = "http://gcc.genscape.com/GCCcontent/intranet/manual_normalization.php";
 
-// Select source
-sources = document.getElementById("selFFPipeline");
+// Select form
+mainForm = document.getElementsByName("frmMain")[0]
+
+// Select pipeline
+pipeMenu = document.getElementById("selFFPipeline");
+pipeMenu.value = 248
+
+// Grab all the pipeline ids and associated indexes
+for (var i = 0; i <= pipeMenu.length; i++) {
+    pipeMenu.selectedIndex = i;
+    var pipeID = pipeMenu.value;
+    console.log(i + ":" + pipeID)
+}
+
+// Select opavail run
 table = document.getElementById("reportResultTable");
+
+// Fetch
+fetcher = document.getElementsByName("cmdFF")[0]
