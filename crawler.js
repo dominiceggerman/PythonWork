c5ccb030b8235a525417bbe860d367f5a4289bc8
dominiceggerman <dominiceggerman@gmail.com>
2018-08-14 22:30:23 -0500

    Added date range generator

--- crawler.js
+++ crawler.js
@@ -11,23 +11,46 @@ mainForm = document.getElementsByName("frmMain")[0];
 pipeMenu = document.getElementById("selFFPipeline");
 pipeMenu.value = 248;
 
-///////////////////////
-// Select date range //
-///////////////////////\
-dates = ["08/08/2018", "08/09/2018", "08/10/2018"]
-dateEntry = document.getElementById("txtFFDateIn")
-for (var i = 0; i < date.length; i++) {
-    dateEntry.value = dates[i]
+// Returns an array of dates between the two dates
+var getDates = function(startDate, endDate) {
+    var dates = [],
+        currentDate = startDate,
+        addDays = function(days) {
+            var date = new Date(this.valueOf());
+            date.setDate(date.getDate() + days);
+            return date;
+        };
+    while (currentDate <= endDate) {
+        dates.push(currentDate);
+        currentDate = addDays.call(currentDate, 1);
+    }
+    return dates;
 }
-////////////////////////
-// Select opavail run //
-////////////////////////
+// Usage - dates are YYYY, MM, DD
+var dates = getDates(new Date(2013,10,22), new Date(2013,11,25));
+var dateArr = [];                                                                                                          
+dates.forEach(function(date) {
+    var day = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
+    dateArr.push(day);
+});
+
+//
+// Select date range
+//
+dates = ["08/08/2018", "08/09/2018", "08/10/2018"];
+dateEntry = document.getElementById("txtFFDateIn");
+for (var i = 0; i < date.length; i++) {
+    dateEntry.value = dates[i];
+}
+//
+// Select opavail run
+//
 // Get the data table
-var table = document.getElementById("reportResultTable")
+var table = document.getElementById("reportResultTable");
 // Check which input has manual run = True
 var numOps = table.children[1].children.length;
 // Instantiate input
-var input
+var input;
 // Loop to get the correct input
 for (var i = 0; i <= numOps; i++) {
     var args = table.children[1].children[i].children[2].textContent;
@@ -39,12 +62,20 @@ for (var i = 0; i <= numOps; i++) {
 }
 
 // Mimic click on input
-input.click()
+input.click();
 
 // Fetch
 var fetcher = document.getElementById("cmdFF");
 fetcher.click();
 
-/////////////////////////////
-// Look for status message //
-/////////////////////////////
+//
+// Look for status message
+//
+var statusTable = document.getElementsByTagName("table")[0];
+var status = statusTable.children[0].children[2].children[2].textContent;
+if (status.includes("completed")) {
+    outputDetail = document.getElementsByTagName("textarea")[0].textContent;
+    if (outputDetail.includes("errors=0;")) {
+        console.log(outputDetail);
+    }
+}
