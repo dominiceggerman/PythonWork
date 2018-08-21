e28a98362cf74fa9188bf1e38c5fc6c2397bf3f8
Dominic Eggerman <dominiceggerman@gmail.com>
2018-08-21 10:49:43 -0500

    Moved demo-lgin to access DB.

---
+++ accessDB.py
@@ -0,0 +1,20 @@
+# By Dominic Eggerman
+# Imports
+import getpass
+import sqlalchemy
+
+# Connect to database
+def connect(user, password):
+    pass
+
+# Query database
+def query():
+    pass
+
+
+# Run
+if __name__ == "__main__":
+
+    username = input('Enter username: ')
+    password = getpass.getpass('Enter password: ')
+    connect(username, password)
