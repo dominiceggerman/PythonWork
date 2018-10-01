13f662ad39f0cee64f21974553b7bacb11aba8df
dominiceggerman <dominiceggerman@gmail.com>
2018-10-01 14:13:33 -0500

    Added logic to allow selection of all datasets.

--- selenium-test.py
+++ selenium-test.py
@@ -22,6 +22,10 @@ def dateRangeGenerator():
 if __name__ == '__main__':
     # List of strings for status of run
     log = []
+    # Select dataset
+    dataset = input("Select dataset (opavail / gas_quality / no_notice etc.): ")
+    if ("opavail", "no_notice", "gas_quality", "segment_capacity", "index_of_customers") not in dataset:
+        raise Exception("Select a dataset to force fetch (opavail, no_notice, gas_quality, segment_capacity, index_of_customers)")
 
     # Get the date range
     dates = dateRangeGenerator()
@@ -34,52 +38,91 @@ if __name__ == '__main__':
         # Wait and check for this string in title
         time.sleep(2)
         assert "Manual Operations" in driver.title
+
+        # Select pipeline
+        pipe_id = input("Input pipeline ID: ")
         # Wait, find the source box, select source
-        time.sleep(2)
+        time.sleep(1)
         source_elem = driver.find_element_by_css_selector("#selFFPipeline")
-        source_elem.send_keys("407")
+        source_elem.send_keys("{0}".format(pipe_id))
         time.sleep(0.1)
         source_elem.send_keys(Keys.RETURN)
-        # Wait, find the date box, change date
-        time.sleep(2)
-        date_elem = driver.find_element_by_css_selector("#txtFFDateIn")
-        driver.execute_script("arguments[0].setAttribute('value','09/25/2018')", date_elem)
-        # Wait, find the job with the desired dataset, click it
-        time.sleep(2)
-        job_table = driver.find_element_by_css_selector("#reportResultTable")
-        jobs = job_table.find_element_by_tag_name("tbody").find_elements_by_tag_name("tr")  # Get table rows
 
-        # Find opavail arg
-        for j in jobs:
-            arg = j.find_elements_by_tag_name("td")[2]
-            if arg.text == "manualRun=true":
-                # Select the job as an option
-                option = j.find_elements_by_tag_name("td")[0].find_element_by_tag_name("input")
-                # Click it
-                option.click()
-                break
-
-        # Click fetch
-        fetcher = driver.find_element_by_id("cmdFF")
-        fetcher.click()
-        time.sleep(10)
-        
-        # Loop until status is seen
-        # Code for undefined table ??
-
-        # Switch driver to iFrame
-        driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[1])
-        # Check for completed status message
-        while driver.find_element_by_class_name("statusMessageSuccess").text in ("Not started yet", "Fetcher in Progress", "Normalizer in Progress", "Completed Normalizer", "Loader in Progress"):
+        # Loop over the dates that were entered
+        dates = dateRangeGenerator()
+        for date in dates:
+            # Wait, find the date box, change date
+            time.sleep(1)
+            date_elem = driver.find_element_by_css_selector("#txtFFDateIn")
+            driver.execute_script("arguments[0].setAttribute('value','{0}')".format(date), date_elem)
+            # Wait, find the job with the desired dataset, click it
             time.sleep(2)
+            job_table = driver.find_element_by_css_selector("#reportResultTable")
+            jobs = job_table.find_element_by_tag_name("tbody").find_elements_by_tag_name("tr")  # Get table rows
 
-        # Status
-        status_message = driver.find_element_by_class_name("statusMessageSuccess").text
-        print(status_message)
-        log.append(status_message)
+            # Find job
+            for j in jobs:
+                # Get dataset text and arg text
+                dset, arg = j.find_elements_by_tag_name("td")[0], j.find_elements_by_tag_name("td")[2]
 
-        time.sleep(2)
-        driver.close()
+                # opavail
+                if dataset == "opavail":
+                    if dset.text == dataset and arg.text == "manualRun=true":
+                        # Select the job as an option
+                        option = j.find_elements_by_tag_name("td")[0].find_element_by_tag_name("input")
+                        # Click it
+                        option.click()
+                        break
+
+                # no_notice
+                if dataset == "no_notice"
+                    if dset.text == dataset and arg.text == "":
+                        option = j.find_elements_by_tag_name("td")[0].find_element_by_tag_name("input")
+                        option.click()
+                        break
+
+                # gas_quality
+                if dataset == "gas_quality"
+                    if dset.text == dataset and arg.text == "":
+                        option = j.find_elements_by_tag_name("td")[0].find_element_by_tag_name("input")
+                        option.click()
+                        break
+
+                # segment_capacity
+                if dataset == "segment_capacity"
+                    if dset.text == dataset and arg.text == "":
+                        option = j.find_elements_by_tag_name("td")[0].find_element_by_tag_name("input")
+                        option.click()
+                        break
+                
+                # index_of_customers
+                if dataset == "index_of_customers"
+                    if dset.text == dataset and arg.text == "":
+                        option = j.find_elements_by_tag_name("td")[0].find_element_by_tag_name("input")
+                        option.click()
+                        break
+
+            # Click fetch
+            fetcher = driver.find_element_by_id("cmdFF")
+            fetcher.click()
+            time.sleep(10)
+            
+            # Loop until status is seen
+            # Code for undefined table ??
+
+            # Switch driver to iFrame
+            driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[1])
+            # Check for completed status message
+            while driver.find_element_by_class_name("statusMessageSuccess").text in ("Not started yet", "Fetcher in Progress", "Normalizer in Progress", "Completed Normalizer", "Loader in Progress"):
+                time.sleep(2)
+
+            # Status
+            status_message = driver.find_element_by_class_name("statusMessageSuccess").text
+            print(status_message)
+            log.append(status_message)
+
+            time.sleep(2)
+            driver.close()
     
     except KeyboardInterrupt:
         # Close the driver
