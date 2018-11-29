607901356893de4090ddf9194f8d43c56b96c3a8
dominiceggerman <dominiceggerman@gmail.com>
2018-11-29 16:09:02 -0600

    Extra lambda example

--- filterMethod.py
+++ filterMethod.py
@@ -12,3 +12,10 @@ if __name__ == "__main__":
     # Can pass a function or lambda to filter, which will filter out and return boolean values
     print("Filter object: ", filter(lambda num: num % 2 == 0, seq))
     print("Cast as list: ", list(filter(lambda num: num % 2 == 0, seq)))
+
+    print("-- -- --")
+
+    # Example of conditional in lambda
+    seq2 = ['soup','dog','salad','cat','great']
+    print("String sequence to filter: ", seq2)
+    print("Lambda filter with conditional: ", list(filter(lambda st:"s" in st, seq2)))
