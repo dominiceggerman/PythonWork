2a994992d0042e2363de4783366be37f87a38ca4
dominiceggerman <dominiceggerman@gmail.com>
2019-01-02 11:14:00 -0600

    Decision tree work.

--- Decision Trees/treesAndForests.py
+++ Decision Trees/treesAndForests.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cross_validation import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
 
 if __name__ == "__main__":
     # Read Data
@@ -18,6 +20,21 @@ if __name__ == "__main__":
     x = df.drop("Kyphosis", axis=1)
     y = df["Kyphosis"]
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
     # Classify in decision tree
+    print("Decision Tree:")
     dtree = DecisionTreeClassifier()
     dtree.fit(x_train, y_train)
+    dtree_pred = dtree.predict(x_test)
+    print(confusion_matrix(y_test, dtree_pred))
+    print()
+    print(classification_report(y_test, dtree_pred))
+
+    # Random forest
+    print("Random Forest:")
+    rfc = RandomForestClassifier(n_estimators=200)
+    rfc.fit(x_train, y_train)
+    rfc_pred = rfc.predict(x_test)
+    print(confusion_matrix(y_test, rfc_pred))
+    print()
+    print(classification_report(y_test, rfc_pred))
